8f984b035d9b1b47164eb6ffe51f33b195bd1c77
karrk <101902544+karrk@users.noreply.github.com>
2024-03-21 13:13:46 +0900

    no message

---
+++ BOJ/BasicIO/10869/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(inputs[0])+ int.Parse(inputs[1]));
+            Console.WriteLine(int.Parse(inputs[0])- int.Parse(inputs[1]));
+            Console.WriteLine(int.Parse(inputs[0])* int.Parse(inputs[1]));
+            Console.WriteLine(int.Parse(inputs[0])/ int.Parse(inputs[1]));
+            Console.WriteLine(int.Parse(inputs[0])% int.Parse(inputs[1]));
+        }
+
+    }
+}
