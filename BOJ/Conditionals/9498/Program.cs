ec3412df2b529dc4a488b83e1b6808499f7eab81
karrk <101902544+karrk@users.noreply.github.com>
2024-03-21 13:26:53 +0900

    no message

---
+++ BOJ/Conditionals/9498/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Study
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine().ToString());
+
+            switch (num)
+            {
+                case int n when (n >= 90):
+                Console.WriteLine("A");
+                    break;
+                case int n when (n >= 80):
+                    Console.WriteLine("B");
+                    break;
+                case int n when (n >= 70):
+                    Console.WriteLine("C");
+                    break;
+                case int n when (n >= 60):
+                    Console.WriteLine("D");
+                    break;
+                default:
+                    Console.WriteLine("F");
+                    break;
+            }
+        }
+    }
+}
